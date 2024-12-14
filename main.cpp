0a5c1683aab04611a738f863191c30a0cf602196
duck4i <mihailogazda@gmail.com>
2024-12-14 13:34:36 +0000

    logging

--- main.cpp
+++ main.cpp
@@ -12,11 +12,20 @@ int main(int argc, char *argv[])
 
     std::cout << "GGML backend loaded" << std::endl;
 
+    std::string sys_info = llama_print_system_info();
+    std::cout << sys_info << std::endl;
+
+    llama_log_set([](ggml_log_level level, const char *text, void * /*user_data*/) {
+        if (level >= GGML_LOG_LEVEL_WARN && text != nullptr)
+        {
+            printf("%s", text);
+        }
+    }, NULL);
+
     int n_predict = 32; // number of tokens to predict
     llama_model_params model_params = llama_model_default_params();
 
     llama_model *model = llama_load_model_from_file(model_path.c_str(), model_params);
-
     if (model == NULL)
     {
         fprintf(stderr, "%s: error: unable to load model\n", __func__);
@@ -24,6 +33,8 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "Model loaded OK." << std::endl;
+
+    llama_free_model(model);
 
     return 0;
 }
